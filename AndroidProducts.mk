0fe0c5c5b7eaf6d236ed26243cea8d3e7cc60882
SumaPhone <128268582+SumaPhone@users.noreply.github.com>
2024-04-29 09:35:56 +0900

    Clean up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,10 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+LOCAL_PATH := device/zte/P809A23
+
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_P809A23.mk
-
-COMMON_LUNCH_CHOICES := \
-    omni_P809A23-user \
-    omni_P809A23-userdebug \
-    omni_P809A23-eng
